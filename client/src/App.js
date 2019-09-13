87cc49b5854ee332283b441d3b5d3678d32682cd
Rhonda Lawrence <rhonda.lawrence@verizon.com>
2019-09-13 10:51:56 -0500

    commit courses, signin/up headers showing

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import { createCipher } from 'crypto';
 import axios from 'axios';
+import Public from './components/Public';
+import NotFound from './components/NotFound';
+import UserSignUp from './components/UserSignUp';
+import UserSignIn from './components/UserSignIn';
+import UserSignOut from './components/UserSignOut';
+import Authenticated from './components/Authenticated';
+
+
+import withContext from './Context';
+import PrivateRoute from './PrivateRoute';
 
 import {
   BrowserRouter as Router,
@@ -15,9 +25,16 @@ import Header from './components/Header';
 
 import Courses from './components/Courses';
 
-import withContext from './Context';
+//import withContext from './Context';
 
 const HeaderWithContext = withContext(Header);
+//import withContext from './Context';
+//import PrivateRoute from './PrivateRoute';
+//const HeaderWithContext = withContext(Header);
+const AuthWithContext = withContext(Authenticated);
+const UserSignUpWithContext = withContext(UserSignUp);
+const UserSignInWithContext = withContext(UserSignIn);
+const UserSignOutWithContext = withContext(UserSignOut);
 
 export default class App extends Component {
 
@@ -37,7 +54,14 @@ export default class App extends Component {
                <HeaderWithContext />
 
                <Switch>
-//                <Route exact path="/" component={Courses} />
+                 
+                  <Route exact path="/" component={Public} />
+                  <Route path="/courses" component={Courses} />
+                  <PrivateRoute path="/authenticated" component={AuthWithContext} />
+                  <Route path="/signin" component={UserSignInWithContext} />
+                  <Route path="/signup" component={UserSignUpWithContext} />
+                  <Route path="/signout" component={UserSignOutWithContext} />
+                  <Route component={NotFound} />
                </Switch>
             </div>
           </Router>  
